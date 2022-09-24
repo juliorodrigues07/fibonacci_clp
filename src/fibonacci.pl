178fd7394dd7e5ec828d568f4993619edef74244
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-09-23 21:56:10 -0300

    fix: Corrigido caso base da recursão

--- src/fibonacci.pl
+++ src/fibonacci.pl
@@ -1,11 +1,11 @@
+
 % Retorna o n-ésimo termo da sequência de Fibonacci
 
-fibonacci(1, 1).
-fibonacci(2, 1).
-fibonacci(N, Result) :- fibonacci_tail(N, 1, 1, Result).
+fibonacci(N, Result) :- fibonacci_tail(N, 0, 1, Result).
 
+fibonacci_tail(0, N, _, N).
 fibonacci_tail(N, Parcial_1, Parcial_2, Result) :-
-   N > 2,
+   N > 0,
    New_Parcial_2 is Parcial_1 + Parcial_2,
    N1 is N - 1,
    fibonacci_tail(N1, Parcial_2, New_Parcial_2, Result).
